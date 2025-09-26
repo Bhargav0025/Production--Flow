38e3761c76bd6898fa4b42318f1544203fa9f758
bhargav-git <simplebhargav81@gmail.com>
2025-09-26 21:07:26 +0530

    Test jenkinsfile trigger

--- terraform-azure-vm/backend.tf
+++ terraform-azure-vm/backend.tf
@@ -4,5 +4,6 @@ terraform {
     storage_account_name = "tfstatestorage1234"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
+    sas_token            =  "sp=r&st=2025-09-26T13:50:15Z&se=2025-09-26T22:05:15Z&sv=2024-11-04&sr=c&sig=MrVWKxiQT67bSTvM%2FjMHP8ePDcvM7ADekYX2Zy65ehU%3D"
   }
 }
